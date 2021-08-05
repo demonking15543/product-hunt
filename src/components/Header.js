199d6f17a3be52fab2c56b074ca0ebdffc57c7fc
demonking15543 <demonking15543@gmail.com>
2021-08-05 21:57:04 +0530

    How to build product and render it into ProductList learnt

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <h1 className="text-center my-5">Product Hunt</h1>
+            <hr className="my-5"></hr>
+        </div>
+    )
+}
---
+++ src/components/ProductList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Products from './Products'
+
+export default function ProductList() {
+    return (
+        <div>
+        <div className="row">
+            <div className="col-sm-12 text-left">
+                <Products />
+            </div>
+
+            
+        </div>
+        </div>
+    )
+}
